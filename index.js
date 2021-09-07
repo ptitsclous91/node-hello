80f0a387eb474dccee8e0fef85dd66b949a64866
Les <les.ptitsclous@laposte.net>
2021-09-07 09:31:36 +0200

    .

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
- /*
-var express = require("express");
-var app = express();
-
-app.get("/url", (req, res, next) => {
-  res.json(["AAAA","BBBB","CCCC","DDDD","EEEE"]);
- });
-
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
-*/
-
 const http = require('http');
 const port = process.env.PORT || 3000;
 
